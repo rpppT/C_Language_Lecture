5de331da349c07ffb77ae1924e91684190b79732
rpppT <36104309+rpppT@users.noreply.github.com>
2018-08-05 00:17:19 +0900

    Add files via upload

--- 1822.java
+++ 1822.java
@@ -1,43 +1,46 @@
-
+package 알고리즘;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Main {
+public class SET {
+	int n, m;
 	Scanner scanner = new Scanner(System.in);
-	int n_a, n_b;
-	int i;
-
+	Set<Integer> set = new HashSet<>();
 	
-	Set<Integer> set_1 = new HashSet<>(), set_2 = new HashSet<>();
-	
-	public Main() {
-		n_a = scanner.nextInt();
-		n_b = scanner.nextInt();
+	public void func(){
 		
-		for(i = 0; i<n_a; i++) 
-			if(!set_1.add(scanner.nextInt()))
-				System.exit(0);
-		for(i =0 ; i < n_b; i++)
-			if(!set_2.add(scanner.nextInt()))
-				System.exit(0);
-		for(i = 0; i < set_2.size(); i++) {
-			if(set_1.contains(new ArrayList<>(set_2).get(i))) {
-				set_1.remove(new ArrayList<>(set_2).get(i));
-				n_a  -= 1;
+		n = scanner.nextInt();
+		m = scanner.nextInt();
+
+		while(n-- > 0){
+			set.add(scanner.nextInt());
+		}
+		
+		
+		while(m-- > 0 ){
+			int x = scanner.nextInt();
+			if(set.contains(x)){
+				set.remove(x);
 			}
 		}
-		if(n_a == 0 ) {
-			System.out.println(n_a);
-			System.exit(0);
+		if(set.size() == 0 ){
+			System.out.println(0);
+			return;
 		}
-		System.out.println(n_a);
-		for(i = 0; i<set_1.size(); i++)
-		  System.out.print(new ArrayList<>(set_1).get(i)+" ");
-	}	
+		System.out.println(set.size());
+		ArrayList<Integer> list = new ArrayList<>(set);
+		Collections.sort(list);
+		
+		for(int i = 0; i<set.size(); i++){
+			System.out.print(list.get(i)+" ");
+		}
+	}
 	public static void main(String[] args) {
-		new Main();
+		new SET().func();
 	}
 }
+
